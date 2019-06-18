6c9b32df7bd56f276404ed52e91d883ac860499b
xavlios <5066808+xavlios@users.noreply.github.com>
2019-06-18 09:50:29 -0400

    working?

--- cl_grandmas.lua
+++ cl_grandmas.lua
@@ -1,78 +1,68 @@
-ESX = nil
-local PlayerData = {}
+function GetClosestPlayer()
+	print("getting closest player")
+    local players = GetPlayers()
+    local closestDistance = -1
+    local closestPlayer = -1
+    local ply = GetPlayerPed(-1)
+    local plyCoords = GetEntityCoords(ply, 0)
 
-
-Citizen.CreateThread(function ()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(1)
+    for index,value in ipairs(players) do
+        local target = GetPlayerPed(value)
+        if(target ~= ply) then
+            local targetCoords = GetEntityCoords(GetPlayerPed(value), 0)
+            local distance = GetDistanceBetweenCoords(targetCoords['x'], targetCoords['y'], targetCoords['z'], plyCoords['x'], plyCoords['y'], plyCoords['z'], true)
+            if(closestDistance == -1 or closestDistance > distance) then
+                closestPlayer = value
+                closestDistance = distance
+            end
+        end
     end
+print("closestPlayer" .. closestPlayer)
+print("closestDistance" .. closestDistance)
+    return closestPlayer, closestDistance
+end
 
-    while ESX.GetPlayerData() == nil do
-        Citizen.Wait(10)
-    end
+function GetPlayers()
+  local players = {}
+  	for i = 0, 31 do
+      if NetworkIsPlayerActive(i) then
+				table.insert(players, i)
+				print(players)
+			end
+		end
+	return players
+end
 
-    PlayerData = ESX.GetPlayerData()
+local ped, plyDst
 
-    
-end) 
+RegisterCommand('previve', function(source, args, rawCommand)
+print("Command ran")
+ped, plyDst = GetClosestPlayer()
+print("ped is " .. ped)
+print("plyDst is " .. plyDst)
 
-RegisterNetEvent('esx:playerLoaded')
-AddEventHandler('esx:playerLoaded', function(xPlayer)
-    PlayerData = xPlayer
+local rplayer = GetPlayerPed(GetPlayerFromServerId(-1))
+local crplayer = GetEntityCoords(rplayer,true)
+
+
+-- 1392.0 , 3600.99, 38.94 grandmas location
+
+	if( GetDistanceBetweenCoords( crplayer, 1392.0, 3600.99, 38.94 ) < 5)then
+		print("Im in the area")
+		--if IsEntityDead(ped) then
+			if( plyDst < 2)then
+				--check for cash
+				TriggerServerEvent("grandma:buttonSelected",name, button)
+			    --TriggerServerEvent('grandma:revive', GetPlayerServerId(ped))
+			end
+		--else
+		--	print("player not dead")
+		--end
+	end
 end)
 
 
-RegisterCommand("previve", function(source, args, rawCommand)
-    local targetped = GetPedInFront()
-    local coords = GetEntityCoords(targetped)
-
-	    if targetped ~= 0 then
-			local targetplayer = GetPlayerFromPed(targetped)
-			if targetedPlayer ~= -1 then
-				TriggerServerEvent("Checkhealplayer", GetPlayerServerId(targetplayer))
-			end
-		end
-
-	Citizen.CreateThread(function()
-		DoScreenFadeOut(800)
-
-		while not IsScreenFadedOut() do
-			Citizen.Wait(50)
-		end
-
-
-		local formattedCoords = {
-			x = ESX.Math.Round(coords.x, 1),
-			y = ESX.Math.Round(coords.y, 1),
-			z = ESX.Math.Round(coords.z, 1)
-		}
-
-		RespawnPed(targetped, formattedCoords, 0.0)
-
-    	DoScreenFadeIn(800)
-    end)
-    
-end, false)
-
-
-function GetPedInFront()
-	local player = PlayerId()
-	local plyPed = GetPlayerPed(player)
-	local plyPos = GetEntityCoords(plyPed, false)
-	local plyOffset = GetOffsetFromEntityInWorldCoords(plyPed, 0.0, 1.3, 0.0)
-	local rayHandle = StartShapeTestCapsule(plyPos.x, plyPos.y, plyPos.z, plyOffset.x, plyOffset.y, plyOffset.z, 1.0, 12, plyPed, 7)
-	local _, _, _, _, ped = GetShapeTestResult(rayHandle)
-	return ped
-end
-
-
-
-RegisterNetEvent("healplayer")
-AddEventHandler("healplayer", function()
-	local player = PlayerId()
-	local plyPed = GetPlayerPed(player)
-	if DoesEntityExist(plyPed) then
-		plyPed.
-	end
+RegisterNetEvent("grandma:buttonSelected")
+AddEventHandler("grandma:buttonSelected", function()
+	TriggerServerEvent('grandma:revive', GetPlayerServerId(ped))
 end)
--- sv_grandmas.lua
+++ sv_grandmas.lua
@@ -1,4 +1,31 @@
-RegisterServerEvent("Checkhealplayer")
-AddEventHandler("Checkhealplayer", function(player)
-	TriggerClientEvent("healplayer", tonumber(player))
+ESX = nil
+local playersHealing = {}
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterServerEvent('grandma:revive')
+AddEventHandler('grandma:revive', function(target)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+		print("testing")
+		TriggerClientEvent('esx_ambulancejob:revive', target)
+	
+end)
+
+
+RegisterServerEvent("grandma:buttonSelected")
+AddEventHandler("grandma:buttonSelected", function(name, button)
+
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	mymoney = 5000
+	
+		if mymoney <= xPlayer.getMoney() then
+			TriggerClientEvent("grandma:buttonSelected", source)
+			xPlayer.removeMoney(mymoney)
+			
+		else
+			
+		end
+	
 end)
