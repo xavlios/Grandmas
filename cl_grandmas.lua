aab6fa639cd110a55ba057e8e26dafaf1d981fdd
xavlios <5066808+xavlios@users.noreply.github.com>
2019-09-24 07:21:02 -0400

    Update cl_grandmas.lua

--- cl_grandmas.lua
+++ cl_grandmas.lua
@@ -1,3 +1,17 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(0)
+    end
+
+    while ESX.GetPlayerData().job == nil do
+        Citizen.Wait(100)
+    end
+
+end)
+
 function GetClosestPlayer()
 	print("getting closest player")
     local players = GetPlayers()
@@ -27,56 +41,34 @@ function GetPlayers()
   	for i = 0, 31 do
       if NetworkIsPlayerActive(i) then
 				table.insert(players, i)
-				print(players)
+				--print(players)
 			end
 		end
 	return players
 end
 
 local ped, plyDst
+function RespawnPed(ped, coords, heading)
+    SetEntityCoordsNoOffset(ped, coords.x, coords.y, coords.z, false, false, false, true)
+    NetworkResurrectLocalPlayer(coords.x, coords.y, coords.z, heading, true, false)
+    SetPlayerInvincible(ped, false)
+    TriggerEvent('playerSpawned', coords.x, coords.y, coords.z)
+    ClearPedBloodDamage(ped)
 
+    ESX.UI.Menu.CloseAll()
+end
 RegisterCommand('previve', function(source, args, rawCommand)
-print("Command ran")
-ped, plyDst = GetClosestPlayer()
-print("ped is " .. ped)
-print("plyDst is " .. plyDst)
-
-local rplayer = GetPlayerPed(GetPlayerFromServerId(-1))
-local crplayer = GetEntityCoords(rplayer,true)
-
-
--- 1392.0 , 3600.99, 38.94 grandmas location
-
-	if( GetDistanceBetweenCoords( crplayer, 1392.0, 3600.99, 38.94 ) < 5)then
-		print("Im in the area")
-		if IsEntityDead(ped) then
-            print("player is dead")
-			if( plyDst < 2)then
-				--check for cash
-				TriggerServerEvent("grandma:buttonSelected",name, button)
-			    --TriggerServerEvent('grandma:revive', GetPlayerServerId(ped))
-			end
-		else
-			print("player not dead")
-	   end
-	end
-end)
-
-
-RegisterNetEvent("grandma:buttonSelected")
-AddEventHandler("grandma:buttonSelected", function()
-	TriggerServerEvent('grandma:revive', GetPlayerServerId(ped))
-end)
-
-
-
-RegisterNetEvent('xav_grandmas:revive')
-AddEventHandler('xav_grandmas:revive', function()
+--print("Command ran")
+-----------------------------------------------
+ local rplayer = GetPlayerPed(GetPlayerFromServerId(-1))
+ local crplayer = GetEntityCoords(rplayer,true)
+-- print("player is" .. rplayer .. " and coords x is " .. crplayer.x)
+if( GetDistanceBetweenCoords( crplayer, 1392.0, 3600.99, 38.94 ) < 5)then
     local playerPed = PlayerPedId()
     local coords = GetEntityCoords(playerPed)
 
     TriggerServerEvent('esx_ambulancejob:setDeathStatus', false)
-
+    TriggerServerEvent("grandma:buttonSelected",name, button)
     Citizen.CreateThread(function()
         DoScreenFadeOut(800)
 
@@ -99,4 +91,10 @@ AddEventHandler('xav_grandmas:revive', function()
         StopScreenEffect('DeathFailOut')
         DoScreenFadeIn(800)
     end)
+end
+end)
+-----------------------------------------------
+RegisterNetEvent("grandma:buttonSelected")
+AddEventHandler("grandma:buttonSelected", function()
+	TriggerServerEvent('grandma:revive', GetPlayerServerId(ped))
 end)
