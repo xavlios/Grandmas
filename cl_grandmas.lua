4ec8552bb9597ed32c69599deddef050f0b9bf9c
xavlios <5066808+xavlios@users.noreply.github.com>
2020-10-25 23:35:09 -0400

    re-write 1

--- cl_grandmas.lua
+++ cl_grandmas.lua
@@ -1,15 +1,17 @@
 ESX = nil
 local PlayerData = {}
 local reviveCost = 5000
+local showcircle = true
+local showBlip = true
 local grandmasLocationx = 1392.0  -- coords as a float ex 112.3
 local grandmasLocationy = 3600.99 -- coords as a float ex 112.3
 local grandmasLocationz = 38.0 -- coords as a float ex 112.3
 -- dont touch these values below
-local HasAlreadyEnteredMarker = false
-local LastZone                = nil
-local CurrentAction           = nil
-local CurrentActionMsg        = ''
-local CurrentActionData       = {}
+-- local HasAlreadyEnteredMarker = false
+-- local LastZone                = nil
+-- local CurrentAction           = nil
+-- local CurrentActionMsg        = ''
+-- local CurrentActionData       = {}
 local amount = 0
 
 Citizen.CreateThread(function()
@@ -17,25 +19,20 @@ Citizen.CreateThread(function()
         TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
         Citizen.Wait(0)
     end
-
 end)
 
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(xPlayer)
   PlayerData = xPlayer  
-
 end)
-
 
 function GetPlayers()
     local players = {}
-
     for i = 0, 31 do
         if NetworkIsPlayerActive(i) then
             table.insert(players, i)
         end
     end
-
     return players
 end
 
@@ -57,90 +54,119 @@ function GetClosestPlayer()
             end
         end
     end
-print("closestPlayer " .. closestPlayer)
-
-print("closestDistance " .. closestDistance)
+	print("closestPlayer " .. closestPlayer)
+	print("closestDistance " .. closestDistance)
     return closestPlayer, closestDistance
 end
 
-
-
-
--- Display markers
-Citizen.CreateThread(function()
-	while true do
+RegisterCommand('cpr', function(source)
+    while true do
 		Citizen.Wait(0)
 		local coords = GetEntityCoords(PlayerPedId())
-
-		--for k,v in pairs(Config.Zones) do
-		--	for i = 1, #v.Pos, 1 do
-				if(1 ~= -1 and GetDistanceBetweenCoords(coords, grandmasLocationx, grandmasLocationy, grandmasLocationz, true) < 100) then
-					DrawMarker(27, grandmasLocationx, grandmasLocationy, grandmasLocationz, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 3.0, 3.0, 3.0, 255, 0, 0, 100, false, true, 2, false, false, false, false)
-				end
-		--	end
-		--end
-	end
-end)
-
--- Enter / Exit marker events
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
-		local coords      = GetEntityCoords(PlayerPedId())
-		local isInMarker  = false
-		local currentZone = nil
-
-		--for k,v in pairs(Config.Zones) do
-			--for i = 1, #v.Pos, 1 do
-				if(GetDistanceBetweenCoords(coords, grandmasLocationx, grandmasLocationy, grandmasLocationz, true) < 3.0) then
-					isInMarker  = true
-					currentZone = k
-					LastZone    = k
-				end
-			--end
-		--end
-		if isInMarker and not HasAlreadyEnteredMarker then
-			HasAlreadyEnteredMarker = true
-			TriggerEvent('esx_grandmas:hasEnteredMarker', currentZone)
-		end
-		if not isInMarker and HasAlreadyEnteredMarker then
-			HasAlreadyEnteredMarker = false
-			TriggerEvent('esx_grandmas:hasExitedMarker', LastZone)
+		-- check to see if your at grandmas
+		if(GetDistanceBetweenCoords(coords, grandmasLocationx, grandmasLocationy, grandmasLocationz, true) < 3.0) then
+			revivePlayer()
 		end
 	end
-end)
+end, false)
 
-AddEventHandler('esx_grandmas:hasEnteredMarker', function(zone)
-	CurrentAction     = 'heal_menu'
-	CurrentActionMsg  = 'Press [E] to start the revive process.'
-	CurrentActionData = {zone = zone}
-end)
 
-AddEventHandler('esx_grandmas:hasExitedMarker', function(zone)
-	CurrentAction = nil
-	ESX.UI.Menu.CloseAll()
-end)
+-- -- Display markers
+-- Citizen.CreateThread(function()
+-- 	while true do
+-- 		Citizen.Wait(0)
+-- 		local coords = GetEntityCoords(PlayerPedId())
+
+-- 		--for k,v in pairs(Config.Zones) do
+-- 		--	for i = 1, #v.Pos, 1 do
+-- 				if(1 ~= -1 and GetDistanceBetweenCoords(coords, grandmasLocationx, grandmasLocationy, grandmasLocationz, true) < 100) then
+-- 					DrawMarker(27, grandmasLocationx, grandmasLocationy, grandmasLocationz, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 3.0, 3.0, 3.0, 255, 0, 0, 100, false, true, 2, false, false, false, false)
+-- 				end
+-- 		--	end
+-- 		--end
+-- 	end
+-- end)
+
+-- -- Enter / Exit marker events
+-- Citizen.CreateThread(function()
+-- 	while true do
+-- 		Citizen.Wait(0)
+-- 		local coords      = GetEntityCoords(PlayerPedId())
+-- 		local isInMarker  = false
+-- 		local currentZone = nil
+
+-- 		--for k,v in pairs(Config.Zones) do
+-- 			--for i = 1, #v.Pos, 1 do
+-- 				if(GetDistanceBetweenCoords(coords, grandmasLocationx, grandmasLocationy, grandmasLocationz, true) < 3.0) then
+-- 					isInMarker  = true
+-- 					currentZone = k
+-- 					LastZone    = k
+-- 				end
+-- 			--end
+-- 		--end
+-- 		if isInMarker and not HasAlreadyEnteredMarker then
+-- 			HasAlreadyEnteredMarker = true
+-- 			TriggerEvent('esx_grandmas:hasEnteredMarker', currentZone)
+-- 		end
+-- 		if not isInMarker and HasAlreadyEnteredMarker then
+-- 			HasAlreadyEnteredMarker = false
+-- 			TriggerEvent('esx_grandmas:hasExitedMarker', LastZone)
+-- 		end
+-- 	end
+-- end)
+
+-- AddEventHandler('esx_grandmas:hasEnteredMarker', function(zone)
+-- 	CurrentAction     = 'heal_menu'
+-- 	CurrentActionMsg  = 'Press [E] to start the revive process.'
+-- 	CurrentActionData = {zone = zone}
+-- end)
+
+-- AddEventHandler('esx_grandmas:hasExitedMarker', function(zone)
+-- 	CurrentAction = nil
+-- 	ESX.UI.Menu.CloseAll()
+-- end)
 
 -- Key Controls
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
+-- Citizen.CreateThread(function()
+-- 	while true do
+-- 		Citizen.Wait(0)
 
-		if CurrentAction ~= nil then
-			ESX.ShowHelpNotification(CurrentActionMsg)
+-- 		if CurrentAction ~= nil then
+-- 			ESX.ShowHelpNotification(CurrentActionMsg)
 
-			if IsControlJustReleased(0, 51) then
-				if CurrentAction == 'heal_menu' then
-					healThem()
-				end
+-- 			if IsControlJustReleased(0, 51) then
+-- 				if CurrentAction == 'heal_menu' then
+-- 					healThem()
+-- 				end
 
-				CurrentAction = nil
-			end
-		else
-			Citizen.Wait(500)
-		end
+-- 				CurrentAction = nil
+-- 			end
+-- 		else
+-- 			Citizen.Wait(500)
+-- 		end
+-- 	end
+-- end)
+
+function revivePlayer()
+	local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
+	if(closestDistance ~= -1 and closestDistance < 5) then
+		-- allow the revive to continue and check if player doing cpr has the money
+		TriggerServerEvent("esx_grandmas:checkMoney")
+	else
+		print("No one else is close to me")
 	end
+end
+
+RegisterNetEvent("esx_grandmas:accepted")
+AddEventHandler("esx_grandmas:accepted", function(newvalue)
+	-- pull up bar to do revive
 end)
+
+RegisterNetEvent("esx_grandmas:rejected")
+AddEventHandler("esx_grandmas:rejected", function(newvalue)
+	
+end)
+
 
 function healThem()
 
@@ -164,33 +190,29 @@ function healThem()
 
 					print("Running heal script")
 
-						ESX.TriggerServerCallback('esx_ambulancejob:getItemAmount', function(quantity)
+					local closestPlayerPed = GetPlayerPed(closestPlayer)
+					local health = GetEntityHealth(closestPlayerPed)
+					local target_id = GetPlayerServerId(closestPlayer)
+					print("the health of the dead player is " .. health .. " the player is " .. closestPlayer .. " id is " .. target_id)
+
+					if health == 0 then
+						local playerPed = PlayerPedId()
+
+						IsBusy = true
+						ESX.ShowNotification("heal in progress")
+						TaskStartScenarioInPlace(playerPed, 'CODE_HUMAN_MEDIC_TEND_TO_DEAD', 0, true)
+						Citizen.Wait(10000)
+						ClearPedTasks(playerPed)
+
+						TriggerServerEvent('esx_grandma:revive', GetPlayerServerId(closestPlayer))
+						----TriggerServerEvent('grandma:heal', target_id, 'big')
+						ESX.ShowNotification("heal complete", GetPlayerName(closestPlayer))
+						IsBusy = false
+						TriggerServerEvent("esx_grandmas:moneyTake", reviveCost)
+					else
+						ESX.ShowNotification("player not dead")
+					end
 							
-								local closestPlayerPed = GetPlayerPed(closestPlayer)
-								local health = GetEntityHealth(closestPlayerPed)
-								local target_id = GetPlayerServerId(closestPlayer)
-								print("the health of the dead player is " .. health .. " the player is " .. closestPlayer .. " id is " .. target_id)
-
-								if health == 0 then
-									local playerPed = PlayerPedId()
-
-									IsBusy = true
-									ESX.ShowNotification("heal in progress")
-									TaskStartScenarioInPlace(playerPed, 'CODE_HUMAN_MEDIC_TEND_TO_DEAD', 0, true)
-									Citizen.Wait(10000)
-									ClearPedTasks(playerPed)
-
-									TriggerServerEvent('esx_grandma:revive', GetPlayerServerId(closestPlayer))
-									----TriggerServerEvent('grandma:heal', target_id, 'big')
-									ESX.ShowNotification("heal complete", GetPlayerName(closestPlayer))
-									IsBusy = false
-									TriggerServerEvent("esx_grandmas:moneyTake", reviveCost)
-								else
-									ESX.ShowNotification("player not dead")
-								end
-							
-						end, 'bandage')
-
 				end)
 				
 			else
--- sv_grandmas.lua
+++ sv_grandmas.lua
@@ -3,6 +3,24 @@ local GrandmasFee = 4000
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+RegisterServerEvent("esx_grandmas:checkMoney")
+AddEventHandler("esx_grandmas:checkMoney", function()
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local money = xPlayer.getMoney()
+	if money >= GrandmasFee then
+		TriggerClientEvent("esx_grandmas:accepted",source, money) -- send accept to client for if needed
+		TriggerEvent("esx_grandmas:removeMoney") -- take the money from the player doing cpr
+	else
+		TriggerClientEvent("esx_grandmas:rejected",source, money)
+	end
+end)
+
+RegisterServerEvent("esx_grandmas:removeMoney")
+AddEventHandler("esx_grandmas:removeMoney", function()
+        local xPlayer = ESX.GetPlayerFromId(source)
+		xPlayer.removeMoney(GrandmasFee)
+end)
+
 
 
 RegisterServerEvent('grandma:heal')
@@ -21,17 +39,5 @@ AddEventHandler('esx_grandma:revive', function(target)
 end)
 
 
-RegisterServerEvent("esx_grandmas:money")
-AddEventHandler("esx_grandmas:money", function()
-        local xPlayer = ESX.GetPlayerFromId(source)
-		local money = xPlayer.getMoney()
 
-		--print("Player has " .. money .. " on them.")
-	TriggerClientEvent("esx_grandmas:amount",source, money)
-end)
 
-RegisterServerEvent("esx_grandmas:moneyTake")
-AddEventHandler("esx_grandmas:moneyTake", function()
-        local xPlayer = ESX.GetPlayerFromId(source)
-		xPlayer.removeMoney(GrandmasFee)
-end)
